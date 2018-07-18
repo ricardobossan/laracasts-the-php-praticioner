cb447510bac2c8f7098196f37b868b196163f888
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-18 14:21:58 -0300

    makes exercise from video 7 meet specifications

--- src/7-associative-arrays/index.php
+++ src/7-associative-arrays/index.php
@@ -1,12 +1,14 @@
 <?php
 
 $task = [
-	'person' => 'Ricardo',
-	'job' => 'Learn PHP',
-	'target' => 'geting hired at Brasil Game Show',
-	'when' => 'ASAP'
+	'title' => 'Learn PHP, for job interview at Brasil Game Show',
+	'assigned_to' => 'Ricardo',
+	'due' => 'ASAP',
+	'completed' => 'false'
 ];
 
 require 'index.view.php';
 
+// To stop reading code
+// unecessary at this point, but I'm saving it here so I remember learning it
 die();
