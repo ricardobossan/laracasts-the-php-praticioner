6173fc3d81f0b97c20a52c524a1c560a4b3f8e2b
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-18 14:50:12 -0300

    apply code for 8-Booleans.mp4, until 04m00s

---
+++ src/8-booleans/index.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>header{background:#e3e3e3;padding:2em;text-align:center;}</style>
+</head>
+<body>
+	<ul>
+		<?php foreach($task as $specification => $value) : ?>
+			<li><strong><?= $specification; ?></strong> <?= $value ?></li>
+		<?php endforeach; ?>
+	 </ul>
+</body>
+</html>
