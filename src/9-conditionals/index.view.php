898fa677eca2fb8140d4e80b317e4d0b50bfc788
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-19 18:41:39 -0300

    Add files for video 9

---
+++ src/9-conditionals/index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>header{background:#e3e3e3;padding:2em;text-align:center;}</style>
+</head>
+<body>
+	<ul>
+		<li>
+			<strong>Name: </strong><?= $task['title']; ?>
+		</li>
+		<li>
+			<strong>Due Date: </strong><?= $task['due']; ?>
+		</li>
+		<li>
+			<strong>Personal Responsible: </strong><?= $task['assigned_to']; ?>
+		</li>
+		<li>
+			<strong>Status: </strong><?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+		</li>
+	</ul>
+</body>
+</html>
