c5c958805fead9308bc3d602dded159f13b538dc
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-22 20:05:18 -0300

    completes code for video 12

--- src/12-classes/index.php
+++ src/12-classes/index.php
@@ -4,21 +4,51 @@
  * @file Todo Application
  */
 
+require 'functions.php';
+
 /**
  * @class todo
  */
  class Task {
 
- 	public function __construct($descrption)
+ 	/* switch these variables ($description and $completedto protected, at more high level*/
+ 	public $description;
+
+ 	public $completed = false;
+
+ 	public function __construct($description)
 
  	{
-
  		//automatically triggered on instantiation
  		$this->description = $description;
  	}
 
+ 	public function isComplete() {
+ 		return $this->completed;
+ 	}
+
+ 	public function complete() {
+ 		return $this->completed = true;
+ 	}
+	/*
+ 	public function describe() { // for use at more high level
+ 		return $this->description;
+ 	}
+	*/
  }
 
- $task = new task('Go to the store');
+/*$task = new task('Go to the store'); // a new task object
+*/
 
+/* or create an array and iterate over it at the index.view.php file */
+$tasks = [
+	new task('Go to the store'),
+	new task('Finish my screencast'),
+	new task('Clean my room')
+];
+
+$tasks[2]->complete();
+
+/*dd($tasks);
+*/
  require 'index.view.php';
--- src/12-classes/index.view.php
+++ src/12-classes/index.view.php
@@ -3,11 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<style>header{background:#e3e3e3;padding:2em;text-align:center;}</style>
+	<style>header{background:#e3e3e3;padding:2em;text-align:justify;}</style>
 </head>
 <body>
-	<ul>
-		<li><?=1animals ?></li>
-	</ul>
+	<header>
+
+		<ul>
+			<?php foreach($tasks as $task) : ?>
+				<li>
+					<?php if($task->completed) : ?>
+						<strike><?= $task->description ?></strike>
+					<?php else : ?>
+						<?= $task->description; ?>
+					<?php endif; ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+
+	</header>
 </body>
 </html>
