916376757f3c9e1d252e82ef54fbb94f800672dd
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-24 09:41:18 -0300

    create the database directory, for being home to anything related to connecting and working with the database. Also create the connection.php file, with the Connection class

---
+++ src/14-pdo-refactoring/database/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file THis file will be home the Connection class and for anything related to connecting to my database.
+ */
+
+/**
+ * Connects my database.
+ * @class
+ */
+class Connection
+{
+	public function make()
+	{
+
+	}
+}
