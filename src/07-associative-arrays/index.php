ee6bce1d4acfecb0c7dd4ad53ed33ec05cce41c5
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-11-10 13:36:55 -0300

    improve lesson 7's code and comments

--- src/07-associative-arrays/index.php
+++ src/07-associative-arrays/index.php
@@ -6,14 +6,21 @@ $person = [
 	'career' => 'web developer'
 ];
 
+// `$variable['key'] = 'value'; appends a value to an array.
 $person['name'] = 'Jeffrey';
 
+// `unset()` Removes a value from the array.
+unset($person['age']);
+
+// `<pre>`, as in _preserve the format_.
 echo '<pre>';
 
+// `var_dump()` dumps the `value` for **anything**, literally.
 var_dump($person);
 
 echo '</pre>';
 
 require 'index.view.php';
 
+// `die()` stops whatever comes afterwards.
 die();
--- src/07-associative-arrays/index.view.php
+++ src/07-associative-arrays/index.view.php
@@ -3,13 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<style>header{background:#e3e3e3;padding:2em;text-align:center;}</style>
 </head>
 <body>
 	<ul>
 		<?php foreach($person as $feature => $value) : ?>
 			<li><strong><?= $feature; ?></strong> <?= $value ?></li>
 		<?php endforeach; ?>
-	 </ul>
+	</ul>
 </body>
 </html>
