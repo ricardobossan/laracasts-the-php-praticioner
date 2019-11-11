26952793a7c5a7b7976dbc6aae4ec5c3a3c5a845
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-11-11 16:48:44 -0300

    improve lesson 11's code and comments

--- src/11-mysql/index.php
+++ src/11-mysql/index.php
@@ -1,42 +1,41 @@
-	<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<style>
+		pre {
+			background-color:lightgray;
+		}
 
-echo '<pre>';
-echo 'SETTING UP MYSQL ON THE COMMAND LINE:';
-echo '</pre>';
-// log in with the root user name. For root user, generaly there's no password.
-// chose the user before you connect to mysql. I found that, once connected, you cannot switch to another user. You have to `exit` mysql and connect again with the other user;
-echo 'mysql -u root';
-// in general, your database correspondes to yoru website. 1 site, 1 database.
-echo 'create database mytodo
-<br>';
-echo 'show databases
-<br>';
-echo 'use mytodo
-<br>';
-/*echo 'create table todos (description text, completed boolean)
-<br>';
-echo 'show tables
-<br>';
-echo 'describe todos
-<br>';
-*/echo 'drop table todos
-<br>';
-echo 'show tables
-<br>';
-echo 'create table todos (id integer PRIMARY KEY AUTO_INCREMENT, description text NOT NULL
-completed boolean)
-<br>';
-echo 'show tables
-<br>';
-echo 'describe todos
-<br>';
-echo 'insert into todos (description, completed) values("Go to the store", false)
-<br>';
-echo 'select * from todos
-<br>';
-echo 'Install a GUI for mySQL
-<br>';
-echo 'I installed "HeidSQL"
-<pre>';
-echo 'END OF VIDEO
-</pre>';
+		li {
+			font-style:italic;
+		}
+	</style>
+</head>
+<body>
+	<h1>Setting mySQL CLI</h1>
+	<pre>Log in with the root user name. For root user, generaly there's no password.</pre>
+	<pre>Chose the user before you connect to mysql. I found that, once connected, you cannot switch to another user. You have to `exit` mysql and connect again with the other user;</pre>
+	<pre>Add this path to the path enviroment variable and restart your code editor: C:\`$xamp_root_folder`\mysql\bin.</pre>
+	<ul>
+		<li>mysql -u root</li>
+		<li>create database mytodo;</li>
+		<pre> In general, your database correspondes to your website. 1 site, 1 database.</pre>
+		<li>show databases;</li>
+		<li>use mytodo;</li>
+		<li>create table todos (id integer PRIMARY KEY AUTO_INCREMENT, description text NOT NULL, completed boolean);</li>
+		<pre>the order for each column is `field, type, modifier`, separated from the others by colons.</pre>
+		<li>show tables;</li>
+		<li>describe todos;</li>
+		<li>insert into todos (description, completed) values("Go to the store", false);</li>
+		<pre>the id field/collumn will auto increment, as defined in it's modifier, so it does not need to be added</pre>
+		<li>select * from todos;</li>
+		<li>Install a GUI for mySQL;</li>
+		<li>I installed "HeidSQL"</li>	
+		<li>drop table todos;</li>
+	</ul>
+</body>
+</html>
